173a5031ab781943a998e097e2e9c120de0ae9d0
trippedoutfish <trippedoutfish@gmail.com>
2020-01-29 00:24:08 -0800

    Added rotation pool. People need to join to send messages, this should help reduce people clobbering each other by enforcing a round robin system. Additionally if someone doesn't reply within 30s feel free to steal their spot by sending a message.
    If you are not in the rotation, you need to join with
    !maid join
    
    After that you will have access to
    !maid [get|reply|leave|participants|clear|speaker]
    
    and the shorthands of
    !mr
    !mg
    
    for !maid reply and !maid get respectively
    
    Adding User Secrets Nuget package but not utilizing yet.

--- src/ubad/ubad/Modules/MaidModule.cs
+++ src/ubad/ubad/Modules/MaidModule.cs
@@ -2,6 +2,7 @@
 using Discord.Commands;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using ubad.Services;
 
@@ -9,25 +10,84 @@ namespace ubad.Modules
 {
     public class MaidModule : ModuleBase<SocketCommandContext>
     {
+        
+
         public MaidService MaidService { get; set; }
 
-        private string maxMessage { get; set; }
         private string reply { get; set; }
+        private string maxMessage { get; set; }
 
-        private List<string> messages { get; set; }
 
         [Command("maid", RunMode = RunMode.Async)]
         public async Task GetStory(params string[] objects)
         {
+            if (objects[0] == "join")
+            {
+                if (MaidService.Contains(Context.User as IGuildUser))
+                {
+                    await ReplyAsync($"{Context.User.Username} you silly goose, you are already in the story rotation.");
+                    return;
+                }
+                MaidService.AddParticipant(Context.User as IGuildUser);
+                await ReplyAsync($"{Context.User.Username} you have been added to the story rotation, there  are {MaidService.CountParticipants} people participating,");
+                return;
+            }
+            else if (objects[0] == "leave")
+            {
+                if (!MaidService.Contains(Context.User as IGuildUser))
+                {
+                    MaidService.RemoveParticipant(Context.User as IGuildUser);
+                    await ReplyAsync($"{Context.User.Username} you have been removed from the participants list.");
+                    return;
+                }
+                await ReplyAsync($"{Context.User.Username} you are not participating.");
+                return;
+            }
+
+            if (!MaidService.Contains(Context.User as IGuildUser))
+            {
+                await ReplyAsync($"{Context.User.Username} you silly goose, participate by joining the rotation with **!maid join**.");
+                return;
+            }
+
+
             if (objects[0] == "get")
             {
                 reply = await MaidService.GetResponseAsync();
+                MaidService.advancePaused = false;
+                await DisplayWholeReply();
+                return;
             }
             else if (objects[0] == "reply")
             {
-                reply = await MaidService.PostResponseAsync(string.Join(' ', objects[1..]));
-                await Task.Delay(new TimeSpan(0, 0, 10));
-                reply = await MaidService.GetResponseAsync();
+                if ((MaidService.IsCurrentSpeaker(Context.User as IGuildUser) && !MaidService.advancePaused) || DateTime.UtcNow.Subtract(MaidService.lastAdvancement).TotalSeconds > 30)
+                {
+                    reply = await MaidService.PostResponseAsync(string.Join(' ', objects[1..]));
+                    await Task.Delay(new TimeSpan(0, 0, 10));
+                    reply = await MaidService.GetResponseAsync();
+                    if (reply != "Still Generating...")
+                    {
+                        MaidService.advancePaused = false;
+                        MaidService.AdvanceSpeaker();
+                    }
+                    await DisplayWholeReply();
+                    return;
+                }
+            }
+            else if (objects[0] == "participants")
+            {
+                await ReplyAsync(MaidService.ListParticipants);
+                return;
+            }
+            else if (objects[0] == "clear")
+            {
+                MaidService.Clear();
+                return;
+            }
+            else if (objects[0] == "speaker")
+            {
+                await ReplyAsync($"{MaidService.CurrentSpeaker()} is up!");
+                return;
             }
             else
             {
@@ -35,17 +95,20 @@ namespace ubad.Modules
                 return;
             }
 
+            
+        }
+
+        private async Task DisplayWholeReply()
+        {
             if (reply.Length > 1999)
             {
                 while (reply.Length > 1999)
                 {
                     maxMessage = reply[..1999];
-                    // messages.Add(maxMessage);
                     await ReplyAsync(maxMessage);
                     reply = reply[2000..];
                 }
             }
-            // messages.Add(reply);
             await ReplyAsync(reply);
         }
 
@@ -63,7 +126,50 @@ namespace ubad.Modules
             await ReplyAsync("Not Implemented yet :^)");
         }
 
-        //[Command("maid print")]
+        [Command("mr", RunMode = RunMode.Async)]
+        public async Task PostReply(params string[] objects)
+        {
+            if ((MaidService.IsCurrentSpeaker(Context.User as IGuildUser) && !MaidService.advancePaused) || DateTime.UtcNow.Subtract(MaidService.lastAdvancement).TotalSeconds > 30)
+            {
+                reply = await MaidService.PostResponseAsync(string.Join(' ', objects[1..]));
+                await Task.Delay(new TimeSpan(0, 0, 10));
+                reply = await MaidService.GetResponseAsync();
+                if (reply != "Still Generating...")
+                {
+                    MaidService.advancePaused = false;
+                    MaidService.AdvanceSpeaker();
+                }
+                await DisplayWholeReply();
+                return;
+            }
+            else
+            { 
+                if(DateTime.UtcNow.Subtract(MaidService.lastAdvancement).TotalSeconds > 0)
+                {
+                    await ReplyAsync($"It's not your turn to reply or you need to wait {30 - Math.Floor(DateTime.UtcNow.Subtract(MaidService.lastAdvancement).TotalSeconds)} seconds.");
+                }
+                else
+                {
+                    await ReplyAsync($"It's not your turn, **{MaidService.CurrentSpeaker()}** get typing, you will lose  your turn in {30 - Math.Floor(DateTime.UtcNow.Subtract(MaidService.lastAdvancement).TotalSeconds)} seconds.");
+                }
+            }
+        }
+
+        [Command("mg", RunMode = RunMode.Async)]
+        public async Task GetReply(params string[] objects)
+        {
+            if (!MaidService.Contains(Context.User as IGuildUser))
+            {
+                await ReplyAsync($"{Context.User.Username} you silly goose, participate by joining the rotation with **!maid join**.");
+                return;
+            }
+            reply = await MaidService.GetResponseAsync();
+            MaidService.advancePaused = false;
+            await DisplayWholeReply();
+            return;
+        }
+
+        //[Command("mp")]
         //public async Task PrintMessages()
         //{
         //    foreach(var x in messages)
--- src/ubad/ubad/Services/MaidService.cs
+++ src/ubad/ubad/Services/MaidService.cs
@@ -1,5 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using Discord;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,10 +16,77 @@ namespace ubad.Services
         private readonly HttpClient _http;
         private readonly static string ApiConnectionError = "MAID Api Error, likely that the MAID service is not running, please contact the MAID tier 3 helpdesk.";
 
+
+
+        internal int currentSpeaker { get; set; }
+
+        internal bool advancePaused { get; set; }
+
+
+
+        internal DateTime lastAdvancement { get; set; }
+
+        internal int CountParticipants
+        {
+            get => membersInStory.Count;
+        }
+
+        internal string ListParticipants
+        {
+            get => string.Join(',', membersInStory.Select(x => x.Username).ToList());
+        }
+        private List<IGuildUser> membersInStory { get; set; }
+
+        private List<string> messages { get; set; }
+
+
         public MaidService(HttpClient http)
         {
             http.Timeout = new TimeSpan(0, 0, 5);
             _http = http;
+
+            membersInStory = new List<IGuildUser>();
+            currentSpeaker = 0;
+            advancePaused = false;
+            lastAdvancement = DateTime.UtcNow;
+        }
+
+        public bool Contains(IGuildUser user)
+        {
+            return membersInStory.Contains(user);
+        }
+
+        public void Clear()
+        {
+            membersInStory.Clear();
+            currentSpeaker = 0;
+        }
+        public void AddParticipant(IGuildUser user)
+        {
+            membersInStory.Add(user);
+        }
+
+        public void RemoveParticipant(IGuildUser user)
+        {
+            membersInStory.Remove(user);
+        }
+
+        public bool IsCurrentSpeaker(IGuildUser user)
+        {
+            if(membersInStory.Count == 0)
+            {
+                return false;
+            }
+            return membersInStory[currentSpeaker] == user;
+        }
+
+        public string CurrentSpeaker()
+        {
+            if (membersInStory.Count == 0)
+            {
+                return "Nobody participating";
+            }
+            return membersInStory[currentSpeaker].Mention;
         }
 
         public async Task<string> GetResponseAsync()
@@ -36,8 +105,22 @@ namespace ubad.Services
             return ApiConnectionError;
         }
 
+        internal void AdvanceSpeaker()
+        {
+            if(currentSpeaker + 1 >= membersInStory.Count)
+            {
+                currentSpeaker = 0;
+            }
+            else
+            {
+                currentSpeaker += 1;
+            }
+            lastAdvancement = DateTime.UtcNow;
+        }
+
         public async Task<string> PostResponseAsync(string response)
         {
+            advancePaused = true;
             try
             {
                 var content = new StringContent("{\"response\": \"" + response + "\"}", Encoding.UTF8, "application/json");
